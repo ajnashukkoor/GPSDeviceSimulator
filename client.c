687e862d287f075cf3a4f91ad2cc2e8babb07137
Ajna Shukkoor <ajna.shukkoor@agtstech.com>
2020-06-01 11:15:09 +0300

    simulator and adaptor

--- client.c
+++ client.c
@@ -1,10 +1,64 @@
 // Client side C/C++ program to demonstrate Socket programming 
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
 #define PORT 8080 
+#define MAX 1024
+
+void func(int);
+
+void func(int sockfd)
+{
+    char buff[MAX];
+    int n, valread;
+
+    char *login_response = "7878050100059FF80D0A";
+    char *heartbeat_response = "787805230100670E0D0A";
+//  char *GPSlocation_response = "787822220F0C1D023305C9027AC8180C46586000140001CC00287D001F71000001000820860D0A";
+    char *login_packet = "7878110103516080807792882203320101AA53360D0A";
+    char *heartbeat_packet = "78780B23C00122040001000818720D0A";
+    char *GPSlocation_packet = "787822220F0C1D023305C9027AC8180C46586000140001CC00287D001F71000001000820860D0A";
+
+
+    for (;;) {
+
+	    /*
+	    
+        bzero(buff, sizeof(buff));
+
+        printf("Enter the string : ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        //write(sockfd, buff, sizeof(buff)); 
+	*/
+        printf("Inside loop\n");
+	
+        //read(sockfd, buff, sizeof(buff)); 
+        valread = read( sockfd , buff, 1024);
+        printf("\nFrom Server : %s\n", buff);
+
+	//logic to send appropriate response
+        if ((strncmp(buff, login_packet, strlen(buff))) == 0) 
+	strcpy(buff, login_response);
+        if ((strncmp(buff, heartbeat_packet, strlen(buff))) == 0) 
+	strcpy(buff, heartbeat_response);
+        if ((strncmp(buff, GPSlocation_packet, strlen(buff))) == 0) 
+	strcpy(buff, login_response);
+	printf("buff is now %s\n", buff);
+        send(sockfd , buff , strlen(buff) , 0 );
+        bzero(buff, sizeof(buff));
+
+//        if ((strncmp(buff, "exit", 4)) == 0) {
+//            printf("Client Exit...\n");
+//            break;
+//        }
+    }
+}
+
    
 int main(int argc, char const *argv[]) 
 { 
@@ -12,6 +66,9 @@ int main(int argc, char const *argv[])
     struct sockaddr_in serv_addr; 
     char *hello = "Hello from client"; 
     char buffer[1024] = {0}; 
+    char new[3] = {0};
+
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
         printf("\n Socket creation error \n"); 
@@ -33,9 +90,63 @@ int main(int argc, char const *argv[])
         printf("\nConnection Failed \n"); 
         return -1; 
     } 
+
+    else
+        printf("connected to the server..\n");
+
     send(sock , hello , strlen(hello) , 0 ); 
     printf("Hello message sent\n"); 
+    memset (buffer, '\0', sizeof(buffer) );
     valread = read( sock , buffer, 1024); 
     printf("%s\n",buffer ); 
+    printf("entering to for loop\n" );
+    printf("entering to for loop\n" );
+    printf("entering to for loop\n" );
+    printf("entering to for loop\n" );
+
+        // function for chat
+    func(sock);
+
+/* 
+    while(1)
+    {
+	memset (buffer, '\0', sizeof(buffer) );
+	printf("\nmemsetdone\n ");
+	valread = read( sock , buffer, 1024);
+//	respond(buffer);
+	printf("%s\n\n",buffer );
+
+//	char protocol_number[3] = {0};
+//	strncpy ( protocol_number, buffer+2, 2 );
+//	buffer = &(buffer) & (0xFF << 11);
+//	printf("%s\n\n",buffer);
+
+	printf("protocol_number is %d%d\n",buffer[4],buffer[5]);
+	printf("num: %c%c \n", buffer[4], buffer[5]);
+	char new[1024] = {0};
+	new[0] = buffer[4];
+	new[1] = buffer[5];
+
+	int ret = atoi(new);
+	printf("protocol number = %d",ret);
+	if (ret==11)
+	{
+	send(sock , login_response, strlen(login_response) , 0 );
+	printf("login response sent");
+	}
+	else if(ret==23)
+	{
+	send(sock , heartbeat_response, strlen(login_response) , 0 );
+	printf("heartbeat response sent");
+	}
+	else if(ret==22)
+	{
+//	send(sock , heartbeat_response, strlen(login_response) , 0 );
+	printf("Location data received");
+	}
+	else 
+		return 0;
+
+    }*/
     return 0; 
 } 
--- server.c
+++ server.c
@@ -6,6 +6,70 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #define PORT 8080 
+#define MAX 1024
+
+void func(int);
+
+void func(int sockfd)
+{
+    char buff[MAX];
+    int n = 0, valread;
+    char *login_packet = "7878110103516080807792882203320101AA53360D0A";
+    char *heartbeat_packet = "78780B23C00122040001000818720D0A";
+    char *GPSlocation_packet = "787822220F0C1D023305C9027AC8180C46586000140001CC00287D001F71000001000820860D0A";
+    char *login_response = "7878050100059FF80D0A";
+    char *heartbeat_response = "787805230100670E0D0A";
+
+
+    // infinite loop for chat 
+    for (;;) {
+        bzero(buff, MAX);
+        // and send that buffer to client 
+        //write(sockfd, login_packet, sizeof(login_packet));
+	//send(sockfd , login_packet, strlen(login_packet), 0 );
+
+        // read the message from client and copy it in buffer 
+        //read(sockfd, buff, sizeof(buff));
+        // print buffer which contains the client contents 
+        //printf("From client: %s\t To client : ", buff); 
+	valread = read( sockfd , buff, 1024);
+        printf("response from client is %s\n",buff );
+ //     bzero(buff, MAX);
+
+       //logic to send appropriate packet according to response
+        if (((strncmp(buff, login_response, strlen(buff))) == 0)&&(n==0))
+	{
+		printf("n = %d\n",n);
+	n++;
+        strcpy(buff, heartbeat_packet);
+	}
+	else if((strncmp(buff, login_response, strlen(buff))) == 0)
+        strcpy(buff, GPSlocation_packet);
+
+        if ((strncmp(buff, heartbeat_response, strlen(buff))) == 0)
+        strcpy(buff, login_packet);
+        //if ((strncmp(buff, login_response, strlen(buff))) == 0)
+        printf("Packet to sent is %s\n", buff);
+        send(sockfd , buff , strlen(buff) , 0 );
+
+//	send(sockfd , login_packet, strlen(login_packet), 0 );
+        bzero(buff, sizeof(buff));
+	
+        //n = 0; 
+        // copy server message in the buffer 
+        //while ((buff[n++] = getchar()) != '\n') 
+        //    ; 
+	
+
+/*        // if msg contains "Exit" then server exit and chat ended. 
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }*/
+    }
+}
+
+
 int main(int argc, char const *argv[]) 
 { 
     int server_fd, new_socket, valread; 
@@ -14,7 +78,10 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
     char *hello = "Hello from server"; 
-
+    char *login_packet = "7878110103516080807792882203320101AA53360D0A";
+    char *heartbeat_packet = "78780B23C00122040001000818720D0A";
+    char *GPSlocation_packet = "787822220F0C1D023305C9027AC8180C46586000140001CC00287D001F71000001000820860D0A";
+    
     printf("\n************************************\n");
     printf("\n************Simulator is ON*********\n");
     printf("\n************************************\n");
@@ -59,5 +126,30 @@ int main(int argc, char const *argv[])
     printf("%s\n",buffer ); 
     send(new_socket , hello , strlen(hello) , 0 ); 
     printf("Hello message sent\n"); 
+
+	//sending the 1st login packet
+	send(new_socket, login_packet, strlen(login_packet), 0 );
+
+
+        // Function for chatting between client and server
+    func(new_socket);
+
+
+    /*
+
+    while(1)
+    {
+	memset (buffer, '\0', sizeof(buffer) );	
+	send(new_socket , login_packet, strlen(login_packet), 0 );
+    	printf("login_packet sent\n"); 
+	//memset( &(*login_packet), '\0', sizeof( login_packet ));
+	//fflush(*login_packet);
+	sleep(1);
+    	valread = read( new_socket , buffer, 1024); 
+    	printf("response is %s\n",buffer ); 
+
+    }*/
+
+
     return 0; 
 } 
